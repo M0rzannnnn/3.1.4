05cd16e974611025c8a153f72a71e4ca328bf3cc
Vadim_Vinogradov <voldo90@inbox.ru>
2023-05-13 02:48:16 +0300

    готовое решение 3.1.4

--- src/main/java/ru/vinogradov/kataBoot/model/User.java
+++ src/main/java/ru/vinogradov/kataBoot/model/User.java
@@ -1,16 +1,26 @@
 package ru.vinogradov.kataBoot.model;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
     @Column(name = "name")
     private String name;
 
@@ -20,16 +30,31 @@ public class User {
     @Column(name = "age")
     private Long age;
 
+
+    @ManyToMany (fetch = FetchType.LAZY)
+    @JoinTable (name = "users_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+
     public User() {}
 
-    public User(String name, String lastName, Long age) {
+    public User(String name, String lastName, Long age, String password, String email) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
+        this.email = email;
+        this.password = password;
+
     }
+    public String getEmail() { return email; }
 
     public String getName() {
         return name;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getLastName() {
@@ -38,6 +63,18 @@ public class User {
 
     public Long getAge() {
         return age;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+    public List<Role> getRoles() {
+        return roles;
     }
 
     public Long getId() {
---
+++ src/main/java/ru/vinogradov/kataBoot/service/RoleService.java
@@ -0,0 +1,12 @@
+package ru.vinogradov.kataBoot.service;
+
+import ru.vinogradov.kataBoot.model.Role;
+
+import java.util.List;
+
+public interface RoleService {
+
+    public List<Role> getAllRoles();
+
+    public List<Role> getUniqAllRoles();
+}
